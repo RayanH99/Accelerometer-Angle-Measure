72369c608e98a435da4c052e7fa8ad76ad462310
rayanh99 <rayanh_99@yahoo.com>
2020-04-08 22:01:41 -0400

    Configured ADC, receive accelerometer values, added delay, setclk, and interrupt functions

--- 2DP4Project_hussar9_400142835/Sources/main.c
+++ 2DP4Project_hussar9_400142835/Sources/main.c
@@ -8,15 +8,28 @@
 
 
 
+unsigned short x;
+unsigned int xy_Toggle;
+unsigned int startStop_Toggle;
+
+
+
 void main(void) {
 
   // Set baud rate based on student specifc bus speed(E-clock)
   // Our specific E-clock speed was 4 MHz
   // baud divisor = 4000000/(16 * 19200) = 13.02.. <- less than 5% error off nearest whole number
   SCI_Init(19200);
+ 
+  // ADC Channel configuration
+  ATDCTL1 = 0x4F;		// set for 12-bit resolution (1001111) 
+	ATDCTL3 = 0x88;		// right justified, one sample per sequence
+	ATDCTL4 = 0x01;		// prescaler = 1; 4MHz / (2 * (1 + 1)) == 1MHz
+	ATDCTL5 = 0x26;		// continuous conversion on channel 6 (AN6)
   
-  
-  DDRJ = 0xFF;
+  //Configure on board LED as output, we will use this to show user start/stop state, on = stop, off = measuring/start
+  DDRJ = 0x01; 
+  PTJ = 0x00; //start powered off to show that we are currently in the start/measure state
   
   // Configure PP0-PP7 Pin
   // Access using PTP to output
@@ -47,7 +60,16 @@ void main(void) {
   
   PTP = 0b00000000;
   
+  
+  while(1){
+    
+    x = ATDDR0; // store digital value from accelerometer
 
+    SCI_OutUDec(x);
+    SCI_OutChar(CR);
+  }
+  
+  
 	EnableInterrupts;
 
 
@@ -55,4 +77,58 @@ void main(void) {
     _FEED_COP(); /* feeds the dog */
   } /* loop forever */
   /* please make sure that you never leave main */
+}
+
+
+
+// Function for delay
+void delay1ms(unsigned int multiple){
+  
+  unsigned int i; //loop control variable
+  
+  TSCR1 = 0x90;   //enable timer and fast timer flag clear
+  
+  TSCR2 = 0x00;   //Disable timer interrupt, set prescaler=1
+  
+  TIOS |= 0x01;   //Enable OC0 (not necessary)
+  
+  TC0 = TCNT + 24000;
+   
+  for(i=0;i<multiple;i++) {
+    TFLG2 = 0x80; //clear the TOF flag
+    while (!(TFLG1_C0F));
+    TC0 += 24000;
+  }
+  
+  TIOS &= -0x01; //Disable OC0 (not necessary) 
+  
+}
+
+
+
+// Everything necessary for setting the clock speed
+
+#define VCOFRQ 0x00        //VCOFRQ[1:0]  32MHz <= VCOCLK <= 48MHz
+#define SYNDIV 0x05        //SYNDIV[5:0]  Syn divide is 11
+#define REFFRQ 0x40        //REFFRQ[1:0]  2MHz < fREF <= 6MHz
+#define REFDIV 0x01        //REFDIV[3:0]  Ref Divide is 1
+
+void setClk(void){
+  CPMUPROT = 0x26;               //Protection of clock configuration is disabled
+								 // NOTE: On some Esduinos you may need to use CPMUPROT=0. Try both and see which value allows you to change the registers below in your debugger!
+  
+  CPMUCLKS = 0x80;               //PLLSEL=1. Select Bus Clock Source:  PLL clock or Oscillator.
+  CPMUOSC = 0x80;                //OSCE=1. Select Clock Reference for PLLclk as:fOSC (8 MHz).
+  
+  CPMUREFDIV = REFFRQ+REFDIV;    //Set fREF divider and selects reference clock frequency Range. fREF= 4 MHz.  
+  
+  CPMUSYNR=VCOFRQ + SYNDIV;      //Set Syn divide and selects VCO frequency range. fVCO = 48 MHz.
+  
+  CPMUPOSTDIV=0x00;              //Set Post Divider (0x00= 0000 0000). fPLL= 48 MHz.
+                                 //Overall clock speed is fBUS=fPLL/2=24 MHz  
+  
+  while (CPMUFLG_LOCK == 0) {}  //Wait for PLL to achieve desired tolerance of target frequency. NOTE: For use when the source clock is PLL. comment out when using external oscillator as source clock 
+ 
+ CPMUPROT = 1;                  //Protection for clock configuration is reenabled 
+  //Note: If you change your clock speed you will need to update delay1ms function to give a 1 ms delay
 }
