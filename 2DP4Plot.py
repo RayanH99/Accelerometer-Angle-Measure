b5167b90407c318de24019b40191a5b35f0ebfb3
rayanh99 <rayanh_99@yahoo.com>
2020-04-11 19:45:37 -0400

    final changes, added some more comments to live plotter

--- 2DP4Plot.py
+++ 2DP4Plot.py
@@ -1,3 +1,5 @@
+## close real term before running this module to avoid conflict with the COM port
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import serial
@@ -10,7 +12,7 @@ yAxis = [] ## store accelerometer angles here
 
 def livePlot(i):
     angle = ser.readline()
-    x = int(angle[10:])
+    x = int(angle[10:]) ## take only the value at the end of the serial input to avoid string
 
     xAxis.append(x)
     yAxis.append(len(xAxis))
@@ -18,7 +20,7 @@ def livePlot(i):
     graph.clear()
     graph.plot(yAxis, xAxis)
     graph.set_title("ADXL337 Acclerometer Angle with respect to Time")
-    graph.set_xlabel("Time (seconds/2)")
+    graph.set_xlabel("Time (seconds/2)") ## sampling every half second
     graph.set_ylabel("Angle (degrees)")
 
 liveDisplay = animation.FuncAnimation(display, livePlot, interval = 500)
